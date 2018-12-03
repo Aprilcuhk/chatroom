8b874388cfa6c580043c2b237605c2b2352a9d85
unknown <s_laihuiqian@kingsoft.com>
2018-12-03 17:17:14 +0800

    first commit

---
+++ app/src/main/java/example/com/myapplication/Msg.java
@@ -0,0 +1,25 @@
+package example.com.myapplication;
+
+public class Msg {
+
+    public static final int TYPE_RECEIVE=0;
+
+    public static final int TYPE_SENT=1;
+
+    private String content;
+
+    private int type;
+
+    public Msg(String content,int type){
+        this.content=content;
+        this.type=type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
